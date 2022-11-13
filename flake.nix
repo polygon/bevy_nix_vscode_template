72805ae94a867fdedea4f5e9e72b5726f6941727
polygon <polygon@wh2.tu-dresden.de>
2022-11-13 23:39:19 +0100

    Adapt to bevy 0.9

--- flake.nix
+++ flake.nix
@@ -3,47 +3,112 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     rust-overlay.url = "github:oxalica/rust-overlay";
     nur.url = github:polygon/nur.nix;
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, rust-overlay, nixpkgs, nur }:
+  outputs = { self, rust-overlay, nixpkgs, nur, naersk }:
     let
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+      ];
       overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        inherit overlays;
-      };
-      rust-deps = with pkgs; [
-        (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          targets = [ "wasm32-unknown-unknown" ];
-          extensions = [ "rust-src" ];
-        }))
-        rust-analyzer
-        rustfmt
-        pkgconfig
-        lldb
-        lld
-        clang
-        cargo-geiger
-        nur.packages.x86_64-linux.wasm-server-runner
-      ];
-      runtime-deps = with pkgs; [
-        alsa-lib
-        udev
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        xorg.libxcb
-        libGL
-        vulkan-loader
-        vulkan-headers
-      ];
+      program_name = "bevy_nix_vscode_template";
     in
-    {
-      devShell.x86_64-linux =
-        pkgs.mkShell {
-          buildInputs = rust-deps ++ runtime-deps;
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtime-deps;
-        };
-    };
+    builtins.foldl'
+      (outputs: system:
+
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit overlays system;
+          };
+
+          rust-bin = pkgs.rust-bin.selectLatestNightlyWith
+            (toolchain: toolchain.default.override {
+              targets = [ "wasm32-unknown-unknown" ];
+              extensions = [ "rust-src" ];
+            });
+          naersk-lib = naersk.lib.${system}.override {
+            cargo = rust-bin;
+            rustc = rust-bin;
+          };
+
+          rust-dev-deps = with pkgs; [
+            rust-analyzer
+            rustfmt
+            lldb
+            cargo-geiger
+            nur.packages.${system}.wasm-server-runner
+            renderdoc
+          ];
+          build-deps = with pkgs; [
+            pkgconfig
+            mold
+            clang
+            makeWrapper
+          ];
+          runtime-deps = with pkgs; [
+            alsa-lib
+            udev
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libxcb
+            libGL
+            vulkan-loader
+            vulkan-headers
+          ];
+        in
+        {
+          devShell.${system} =
+            let
+              all_deps = runtime-deps ++ build-deps ++ rust-dev-deps ++ [ rust-bin ];
+            in
+            pkgs.mkShell {
+              buildInputs = all_deps;
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (all_deps);
+              PROGRAM_NAME = program_name;
+              shellHook = ''
+                export CARGO_MANIFEST_DIR=$(pwd)
+              '';
+            };
+          packages.${system} =
+            {
+              app = naersk-lib.buildPackage
+                {
+                  pname = program_name;
+                  root = ./.;
+                  buildInputs = runtime-deps;
+                  nativeBuildInputs = build-deps;
+                  overrideMain = attrs: {
+                    fixupPhase = ''
+                      wrapProgram $out/bin/${program_name} \
+                        --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath runtime-deps} \
+                        --set CARGO_MANIFEST_DIR $out/share/bevy_nix_vscode_template
+                      mkdir -p $out/share/${program_name}
+                      cp -a assets $out/share/${program_name}'';
+                    patchPhase = ''
+                      sed -i s/\"dynamic\"// Cargo.toml
+                    '';
+                  };
+                };
+              wasm = self.packages.${system}.app.overrideAttrs
+                (final: prev: {
+                  CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+                  fixupPhase = '''';
+                });
+            };
+          defaultPackage.${system} = self.packages.${system}.app;
+          apps.${system}.wasm = {
+            type = "app";
+            program = ''${pkgs.writeShellScript "wasm-run" "${nur.packages.${system}.wasm-server-runner}/bin/wasm-server-runner ${self.packages.${system}.wasm}/bin/${program_name}.wasm"}'';
+          };
+        }
+      )
+      { }
+      systems;
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    render::camera::ScalingMode,
+};
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_system(bevy::window::close_on_esc)
+        .add_startup_system(setup)
+        .add_system(rotate)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Camera
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::Auto {
+                min_width: 1.0,
+                min_height: 1.0,
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    // Sprite
+    let img = asset_server.load("bevy.png");
+    commands.spawn(SpriteBundle {
+        texture: img,
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(0.6, 0.6)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
+    for mut tr in &mut query {
+        tr.rotate_local_z(-time.delta_seconds());
+    }
 }
